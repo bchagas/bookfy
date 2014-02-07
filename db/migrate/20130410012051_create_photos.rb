5bdea4b8233bfdb075b31d67290d8e4fd833686c
Bruno Chagas <brunochagasweb@gmail.com>
2014-02-07 12:26:23 -0200

    Updated tables

--- db/migrate/20130410012051_create_photos.rb
+++ db/migrate/20130410012051_create_photos.rb
@@ -2,6 +2,7 @@ class CreatePhotos < ActiveRecord::Migration
   def change
     create_table :photos do |t|
       t.string :name
+      t.integer :album_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,28 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130410014549) do
-
-  create_table "albumphotos", :force => true do |t|
-    t.integer  "photo_id"
-    t.integer  "album_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
+ActiveRecord::Schema.define(:version => 20140207142134) do
 
   create_table "albums", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-  end
-
-  create_table "albums_photos", :id => false, :force => true do |t|
-    t.integer "album_id"
-    t.integer "photo_id"
+    t.integer  "user_id"
   end
 
   create_table "photos", :force => true do |t|
     t.string   "name"
+    t.integer  "album_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
