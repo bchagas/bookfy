4e2dd993a62c4c4aa819fa5fb26ae0dab530dc7d
Bruno Chagas <brunochagasweb@gmail.com>
2014-03-04 21:49:24 -0300

    Added photo title

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  def index
+    if current_user
+      user_id = current_user.id
+      @albums = Album.where(user_id: user_id)
+    end
+  end
+
   private
 
   def current_user
