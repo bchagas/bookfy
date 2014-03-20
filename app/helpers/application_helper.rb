57d0fe424f72245dfd39be795a4933d8d2bc8cb6
Bruno Chagas <brunochagasweb@gmail.com>
2014-03-20 12:49:44 -0400

    Fixed load photo helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,6 @@
 module ApplicationHelper
   def load_photo(id)
-    photo = Photo.find(id)
-    album = Album.find(photo)
-    user = User.find(album.user_id)
-
-    binding.pry
+    user = current_user
     user.instagram.media_item(id).images.standard_resolution.url
   end
 end
