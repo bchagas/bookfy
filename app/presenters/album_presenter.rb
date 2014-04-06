4d1baa56a4d2c819767901651cd15ed3c9830272
Bruno Chagas <brunochagasweb@gmail.com>
2014-04-06 10:15:01 -0300

    Added default album image to empty album

--- app/presenters/album_presenter.rb
+++ app/presenters/album_presenter.rb
@@ -1,9 +1,17 @@
 class AlbumPresenter < ClassicPresenter::Base
   def display_albums
-    if self.empty?
+    if self.empty? and current_user
       context.render "partials/empty_object", object: self
     else
       context.render "partials/albums", albums: self
+    end
+  end
+
+  def display_cover
+    if self.photos.empty?
+      context.image_tag 'default-cover.jpg', alt: 'Album empty'
+    else
+      context.image_tag context.load_photo(self.photos.first.photo_id)
     end
   end
 end
