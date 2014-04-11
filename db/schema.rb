3b108abb8a5a1d3505ec63ae3489374e506143bd
Bruno Chagas <brunochagasweb@gmail.com>
2014-04-11 01:42:17 -0300

    Added position column to photos

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140409010056) do
+ActiveRecord::Schema.define(:version => 20140411025536) do
 
   create_table "albums", :force => true do |t|
     t.string   "name"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(:version => 20140409010056) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.hstore   "data"
+    t.integer  "position"
   end
 
   create_table "users", :force => true do |t|
