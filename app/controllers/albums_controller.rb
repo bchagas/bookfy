929d49b20b7cdc3834cdcdfaff7b69d84d0d40b8
Bruno Chagas <brunochagasweb@gmail.com>
2014-04-23 09:38:49 -0300

    Fixed add photos

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -22,7 +22,7 @@ class AlbumsController < ApplicationController
 
   def new
     @album = Album.new
-    @current_album = Album.friendly_id.find(params[:album_id])
+    @current_album = Album.friendly_id.find(params[:album_id]) if controller_name == 'photos' and action_name == 'new'
     @user_photos.each do |media|
       @album.photos.build(photo_id: media.id)
     end
